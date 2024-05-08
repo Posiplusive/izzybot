3993e326dcce3d0d159361563da1ef4a2358fcf3
Posi+ive <positive105@proton.me>
2024-05-08 21:15:06 +0800

    Implemented a non-hardcoded waifu system

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module izzybot
 go 1.22.2
 
 require (
-	github.com/bwmarrin/discordgo v0.28.1 // indirect
+	github.com/bwmarrin/discordgo v0.28.1
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
--- izzy.go
+++ izzy.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"slices"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"gopkg.in/yaml.v3"
 )
+
+var ostime = time.Now()
 
 func waifuHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	ch, _ := s.State.Channel(m.ChannelID)
@@ -15,10 +20,52 @@ func waifuHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		ch, _ = s.Channel(m.ChannelID)
 	}
 
-	fmt.Printf("Message from @%s in #%+v: %s\n", m.Author.Username, ch.Name, m.Content)
+	sv, _ := s.State.Guild(m.GuildID)
+	if sv == nil {
+		sv, _ = s.Guild(m.GuildID)
+	}
 
-	if m.Content == "!waifu" {
-		s.ChannelMessageSend(m.ChannelID, "Me! (Posi+ive is simping hard for me...)")
+	fmt.Printf("Message in %s from @%s in #%+v: %s\n", sv.Name, m.Author.Username, ch.Name, m.Content)
+
+	file, _ := os.ReadFile("./waifu.yaml")
+
+	if m.Content == "!waifu" { // Honestly this part is just fucking hell
+
+		type UserData struct { // Declares the struct for the yaml
+			UserId []string `yaml:"userid"`
+			Quote  []string `yaml:"quote"`
+		}
+
+		var waifu UserData
+
+		err := yaml.Unmarshal(file, &waifu) // Turns the yaml to a slice and put it in waifu
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		id := m.Author.ID
+		x := slices.Index(waifu.UserId, id) // Returns the index of the matching user ID
+		var y string                        // Declare y var
+
+		if x == -1 { // Puts a random string here so that the
+			y = "undefined" // switch case below goes to the default part
+		} else {
+			y = waifu.UserId[x] // Puts the user ID in a variable for comparison
+		}
+
+		switch id {
+		case y: // if y matches the user,
+			s.ChannelMessageSend(m.ChannelID, waifu.Quote[x]) // then send a message to the user
+		default: // Otherwise just tell them they're not in the list
+			s.ChannelMessageSend(m.ChannelID, "You don't have a waifu yet! Ask my husband to add her for you.")
+		}
+	}
+
+	if m.Content == "!time" {
+		ostime := time.Now()
+		mytime := ostime.Format(time.UnixDate)
+		var timemsg string = fmt.Sprintf("The time at my husband's place is currently %s!", mytime)
+		s.ChannelMessageSend(m.ChannelID, timemsg)
 	}
 }
 
@@ -38,11 +85,12 @@ func main() {
 
 	izzy.AddHandler(waifuHandler)
 
-	izzy.Identify.Intents = discordgo.IntentsGuildMessages
+	izzy.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 
 	izzy.Open()
 
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	fmt.Printf("Bot was started in %s\n", ostime.Format(time.UnixDate))
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
